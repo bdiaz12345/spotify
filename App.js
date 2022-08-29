81d6bd749da387fbc3ae01bc6e069b845222cd7d
bdiaznsd <bdiaz@nationsafedrivers.com>
2022-08-29 16:40:38 -0400

    done

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StatusBar } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { func } from './src/constants';
 
-export default function App() {
+// root stack navigation
+import RootStack from './src/navigation/RootStack';
+
+// app context state
+import AppState from './src/context/AppState';
+
+const App = () => {
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    async function prepare() {
+      try {
+        // keeps the splash screen visible while assets are cached
+        await SplashScreen.preventAutoHideAsync();
+
+        // pre-load/cache assets: images, fonts, and videos
+        await func.loadAssetsAsync();
+      } catch (e) {
+        // console.warn(e);
+      } finally {
+        // loading is complete
+        setIsLoading(false);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  React.useEffect(() => {
+    // when loading is complete
+    if (isLoading === false) {
+      // hide splash function
+      const hideSplash = async () => SplashScreen.hideAsync();
+
+      // hide splash screen to show app
+      hideSplash();
+    }
+  }, [isLoading]);
+
+  if (isLoading) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <AppState>
+      <StatusBar barStyle="light-content" />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      <RootStack />
+    </AppState>
+  );
+};
+
+export default App;
